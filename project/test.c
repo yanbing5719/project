136f1c04fa3acb098eeebc7e14e2c5ca294f68b3
yanbing5719 <2814152946@qq.com>
2025-11-05 21:59:36 +0800

    2025.11.5

--- project/test.c
+++ project/test.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(){
-int a=10;
-printf("%d",a);
-return 0;
+#include <float.h>
+
+int main() {
+    float max_val = FLT_MAX;  // float 类型的最大值
+    float overflow = max_val * 2.0f;
+    
+    printf("最大值: %e\n", max_val);
+    printf("上溢结果: %f\n", overflow);  // 输出: inf 或 infinity
+    
+    return 0;
 }
