24b81dd1b0f46255cbce6c98712274eec7b63dab
yanbing5719 <2814152946@qq.com>
2025-11-08 18:05:39 +0800

    2025.11.8

--- project/test.c
+++ project/test.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-int main(){
-int a,b;
-int c=scanf("%d %d",&a,&b);
-printf("%d",c);
-return 0;
-    }
+#define CAL(a) a * a * a
+#define MAGIC_CAL(a, b) CAL(a) + CAL(b)
+int main(void) {
+  int nums = 1;
+  if(16 / CAL(2) == 2) {
+    printf("I'm TWO(ﾉ>ω<)ﾉ\n");
+  } else {
+    int nums = MAGIC_CAL(++nums, 2);
+    printf("int nums=%d   ",nums);
+  }
+  printf("nums=%d\n", nums);
+}
--- project/链表.c
+++ project/链表.c
@@ -1,198 +1,96 @@
-## 链表的定义和使用
-
-```c
-  
 
 #include <stdio.h>
 
-  
-
 #include <string.h>
-
-  
 
 #include <stdlib.h>
 
-  
-
 struct film{
-
-  
 
 char str[45];
 
-  
-
 struct film * nest;
-
-  
 
 };
 
-  
-
 char * s_gets(char *st,int n);
-
-  
 
 int main(){
 
-  
-
 struct film *head=NULL;
-
-  
 
 struct film *prev=NULL;
 
-  
-
 struct film *current;
 
-  
-
 char input[45];
-
 puts("please input film name");
-
-  
 
 while(s_gets(input,45)!=NULL&&input[0]!='\0'){
 
-  
-
 current=(struct film *)malloc(sizeof(struct film));
-
 strcpy(current->str,input);//初始化新节点
-
 current->nest = NULL;
-
 if(head==NULL){
-
-  
-
 head=current;
-
-  
-
 }else{
-
-  
-
 prev->nest=current;
-
 }
-
 prev=current;
-
 }
-
 current=head;
-
 while(current!=NULL){
 
-  
-
-printf("%s",current->str);
-
-  
+puts(current->str);
 
 current=current->nest;
 
-  
-
 }
-
 current =head;
-
 while(current!=NULL){
-
-struct film *temp=current;
-
-current=current->nest;
-
-free(temp);
-
+    struct film *temp=current;
+    current=current->nest;
+    free(temp);
 }
-
 head=NULL;
-
 return 0;
 
-  
-
 }
 
   
 
-  
-  
-  
+
 
 char * s_gets(char *st,int n){
 
-  
-
 char *ret_val;
-
-  
 
 char *find;
 
-  
-
 ret_val=fgets(st,n,stdin);
-
-  
 
 if(ret_val){
 
-  
-
 find=strchr(st,'\n');
-
-  
 
 if(find){
 
-  
-
 *find='\0';
 
-  
-
 }
-
-  
 
 else{
 
-  
-
 while(getchar()!='\n'){
-
-  
 
 continue;
 
-  
+}
 
 }
 
-  
-
 }
-
-  
-
-}
-
-  
 
 return ret_val;
 
-  
-
 }
-
-```
