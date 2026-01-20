e0d3e74841fbab82664eb134d0284f3b5237bfbb
yanbing5719 <2814152946@qq.com>
2026-01-20 21:31:15 +0800

    2026.1.20

--- project/student.c
+++ project/student.c
@@ -3,117 +3,34 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdio.h>
-/*
-int main(){
-    int mem[]={
-        3,10,3,7,3,8,5,7,6,7,2,3,3,9,9,
-        3,0,8,1,5,8,1,5,2,2,8,1,6,7,0,-15,6,7,9,
-        2,10,4,6,-1,-1,-1,-1,38,7,9,10,11,
-        0,0,0,0,0,0,0,0,
-    };
-    int *p=mem;
-    int *s=mem+sizeof(mem)/sizeof(int)-1;
-    int a=0;
-    for(;;){
-        switch(*p++){
-        case 0:if(!--*s)break;
-        case 1:p+=*p;break;
-        case 2:s-=*p++;break;
-        case 3:*--s=0;
-        case 4:*s+=*p++;break;
-        case 5:s[1]+=s[0];
-        case 6:s++;break;
-        case 7:printf("%d",*s);break;
-        case 8:a=s[0];s[0]=s[*p];s[*p++]=a;break;
-        case 9:printf("\n");break;
-        case 10:
-            for(int i=0;i<8;i++){
-                printf("%d,",mem[sizeof(mem)/sizeof(int)-8+i]);
-                if(i==3){printf("\n");}
-            }
-            printf("\n");
-            break;
-        case 11:
-            return 0;
+struct ListNode {
+     int val;
+      struct ListNode *next;
+  };
+struct ListNode*merge(struct ListNode*list1,struct ListNode*list2){
+    struct ListNode head;
+    struct ListNode* tail =&head;
+    head.next = NULL;
+    while(list1!=NULL&&list2!=NULL){
+        if(list1->val<list2->val){
+            tail->next=list1;
+            list1=list1->next;
+        }else{
+            tail->next=list2;
+            list2=list2->next;
         }
+        tail=tail->next;
     }
+    tail->next=(list1!=NULL)?list1:list2;
+    return head.next;
+ }
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+    if(listsSize==0) return NULL; 
+    if(listsSize==1)return lists[0];
+      struct ListNode*res=merge(lists[0],lists[1]);
+      for(int i=2;i<listsSize;i++){
+      res=merge(res,lists[i]);
+      }
+      return res;
 }
-*/
-
-/*typedef struct student{
-    int id;
-    char name[20];
-    float score[3];
-}stu;
-void input (stu st[],int n);
-void output(stu st[],int n);
-void sort(stu st[],int n);
-void search(stu st[],int n,int num);
-int main(){
-    stu st[10];
-    int n;
-    int num;
-    scanf("%d",&n);
-    scanf("%d",&num);
-   input (st,n);
-   output(st,n);
-   sort(st,n);
-   search(st,n,num);
-
-   return 0;
-}
-void input (stu st[],int n){
-    for(int i=0;i<n;i++){
-       scanf("%d  %s",&st[i].id,&st[i].name);
-       for(int j=0;j<3;j++){
-        scanf("%f",&st[i].score[j]);
-       }
-    }
-}
-void sort(stu st[],int n){
-   float average[3];
-   for(int i=0;i<n;i++){
-    float sum=0;
-   for(int j=0;j<3;j++){
-    sum+=st[i].score[j];
-   }
-   average[i]=sum/3;
-   }
-   for(int i=0;i<n-1;i++){
-    for(int j=i+1;j<n;j++){
-        if(average[i]>average[j]){
-            int t=st[i].id;
-            st[i].id=st[j].id;
-            st[j].id=t;
-            float t1=average[i];
-            average[i]=average[j];
-            average[j]=t1;
-            char t2[20];
-            strcpy(t2,st[i].name);
-            strcpy(st[i].name,st[j].name);
-            strcpy(st[j].name,t2);
-        }
-    }
-   }
-   output(st,n);
-}
-void search(stu st[],int n,int num){
-        int i=num-1;
-     printf("%d  %s  ",st[i].id,st[i].name);
-     for(int j=0;j<3;j++){
-        printf("%f ",st[i].score[j]);
-    }
-    printf("\n");
-    
-}
-void output(stu st[],int n){
-    for(int i=0;i<n;i++){
-    printf("%d  %s  ",st[i].id,st[i].name);
-    for(int j=0;j<3;j++){
-        printf("%f ",st[i].score[j]);
-    }
-    printf("\n");
-   }
-   printf("\n");
-}*/
 
